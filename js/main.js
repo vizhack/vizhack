ef707a2905a09c05dc971547ff05bfa1e3cace8d
GiorgiSheshaberidze <gshes11@freeuni.edu.ge>
2016-12-26 18:53:29 +0400

    fixes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:10,
-        nav:false,
-        autoWidth:true
+        autoWidth:true,
+        nav: true,
+        navText: ["<div class='prev-arrow'></div>","<div class='next-arrow'></div>"]
     });
 });
